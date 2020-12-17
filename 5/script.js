6b65f838448547e2db8a86584ec7b4b5ed19384b
ismaellimadealmeidafilho <58893985+ismaellimadealmeidafilho@users.noreply.github.com>
2020-12-17 00:55:49 -0300

    Conclusão do quinto desafio.
    Conclui o quinto desafio que consistia em selecionar o elemento dom e fazer com que o mesmo fique "rodando", trocando sempre a ultima letra para trás da primeira.

--- 5/script.js
+++ 5/script.js
@@ -1,11 +1,9 @@
 //Write a JavaScript program to rotate the string 'w3resource' in right direction by periodically removing one letter from the end of the string and attaching it to the front.
 
-var texto = document.getElementById('h1');
-var textNode = texto.childNodes[0];
-var text = textNode.data;
-console.log(text)
-//setInterval(() => {
-//    text = text[text.length - 1] + text.substring(0, text.length - 1);
-//  textNode.data = text;   
-//}, 100);
-
+setInterval(() => {
+    var texto = document.getElementById('h1');
+    var textNode = texto.childNodes[0];
+    var text = textNode.nodeValue;
+    text = text[text.length - 1] + text.substring(0, text.length - 1);
+    textNode.nodeValue = text;   
+}, 1000);
